68215cd0625bc77f38f3e04ac4e014274f9d9caf
quvntvn <hulkufe4@gmail.com>
2026-05-17 16:10:32 +0200

    feat(settings): tracking coins_this_run + best_distance persistant

--- hyperdive/scripts/autoload/settings_manager.gd
+++ hyperdive/scripts/autoload/settings_manager.gd
@@ -10,6 +10,8 @@ signal equipped_skin_changed(skin_id: String)
 
 var control_mode: ControlMode = ControlMode.KEYBOARD
 var coins_total: int = 0
+var coins_this_run: int = 0
+var best_distance: int = 0
 var owned_skins: Array[String] = ["default"]
 var equipped_skin: String = "default"
 
@@ -20,8 +22,17 @@ func _ready() -> void:
 
 func add_coin() -> void:
 	coins_total += 1
+	coins_this_run += 1
 	coin_collected.emit(coins_total)
 	save_settings()
+
+func reset_run_stats() -> void:
+	coins_this_run = 0
+
+func update_best_distance(distance: int) -> void:
+	if distance > best_distance:
+		best_distance = distance
+		save_settings()
 
 func buy_skin(skin_id: String) -> bool:
 	var skin: Dictionary = Catalog.get_skin_by_id(skin_id)
@@ -48,6 +59,7 @@ func save_settings() -> void:
 	var cfg: ConfigFile = ConfigFile.new()
 	cfg.set_value("input", "control_mode", control_mode)
 	cfg.set_value("stats", "coins_total", coins_total)
+	cfg.set_value("stats", "best_distance", best_distance)
 	cfg.set_value("cosmetics", "owned_skins", owned_skins)
 	cfg.set_value("cosmetics", "equipped_skin", equipped_skin)
 	cfg.save(SAVE_PATH)
@@ -58,5 +70,6 @@ func load_settings() -> void:
 		return
 	control_mode = cfg.get_value("input", "control_mode", ControlMode.KEYBOARD)
 	coins_total = cfg.get_value("stats", "coins_total", 0)
+	best_distance = cfg.get_value("stats", "best_distance", 0)
 	owned_skins.assign(cfg.get_value("cosmetics", "owned_skins", ["default"]))
 	equipped_skin = cfg.get_value("cosmetics", "equipped_skin", "default")
