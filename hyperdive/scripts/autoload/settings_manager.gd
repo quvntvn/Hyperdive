81cdf105f5a29307dc5c67adcc02774ffc0a5b06
quvntvn <hulkufe4@gmail.com>
2026-05-17 15:47:22 +0200

    feat(settings): persistance coins_total + signal coin_collected

--- hyperdive/scripts/autoload/settings_manager.gd
+++ hyperdive/scripts/autoload/settings_manager.gd
@@ -4,17 +4,25 @@ class_name SettingsManager
 enum ControlMode { KEYBOARD, TOUCH, TILT }
 
 signal control_mode_changed(new_mode: ControlMode)
+signal coin_collected(new_total: int)
 
 var control_mode: ControlMode = ControlMode.KEYBOARD
+var coins_total: int = 0
 
 const SAVE_PATH: String = "user://settings.cfg"
 
 func _ready() -> void:
 	load_settings()
 
+func add_coin() -> void:
+	coins_total += 1
+	coin_collected.emit(coins_total)
+	save_settings()
+
 func save_settings() -> void:
 	var cfg: ConfigFile = ConfigFile.new()
 	cfg.set_value("input", "control_mode", control_mode)
+	cfg.set_value("stats", "coins_total", coins_total)
 	cfg.save(SAVE_PATH)
 
 func load_settings() -> void:
@@ -22,3 +30,4 @@ func load_settings() -> void:
 	if cfg.load(SAVE_PATH) != OK:
 		return
 	control_mode = cfg.get_value("input", "control_mode", ControlMode.KEYBOARD)
+	coins_total = cfg.get_value("stats", "coins_total", 0)
