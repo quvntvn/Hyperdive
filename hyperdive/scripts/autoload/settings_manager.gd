a3c2a90da60ada9a2f69ce7bb0770da95b5598ea
quvntvn <hulkufe4@gmail.com>
2026-05-17 15:58:35 +0200

    feat(settings): owned_skins + equipped_skin + buy_skin + equip_skin + persistance

--- hyperdive/scripts/autoload/settings_manager.gd
+++ hyperdive/scripts/autoload/settings_manager.gd
@@ -5,9 +5,13 @@ enum ControlMode { KEYBOARD, TOUCH, TILT }
 
 signal control_mode_changed(new_mode: ControlMode)
 signal coin_collected(new_total: int)
+signal owned_skins_changed
+signal equipped_skin_changed(skin_id: String)
 
 var control_mode: ControlMode = ControlMode.KEYBOARD
 var coins_total: int = 0
+var owned_skins: Array[String] = ["default"]
+var equipped_skin: String = "default"
 
 const SAVE_PATH: String = "user://settings.cfg"
 
@@ -19,10 +23,33 @@ func add_coin() -> void:
 	coin_collected.emit(coins_total)
 	save_settings()
 
+func buy_skin(skin_id: String) -> bool:
+	var skin: Dictionary = Catalog.get_skin_by_id(skin_id)
+	if coins_total < skin["price"]:
+		return false
+	if skin_id in owned_skins:
+		return false
+	coins_total -= skin["price"]
+	owned_skins.append(skin_id)
+	owned_skins_changed.emit()
+	coin_collected.emit(coins_total)
+	save_settings()
+	return true
+
+func equip_skin(skin_id: String) -> bool:
+	if not skin_id in owned_skins:
+		return false
+	equipped_skin = skin_id
+	equipped_skin_changed.emit(skin_id)
+	save_settings()
+	return true
+
 func save_settings() -> void:
 	var cfg: ConfigFile = ConfigFile.new()
 	cfg.set_value("input", "control_mode", control_mode)
 	cfg.set_value("stats", "coins_total", coins_total)
+	cfg.set_value("cosmetics", "owned_skins", owned_skins)
+	cfg.set_value("cosmetics", "equipped_skin", equipped_skin)
 	cfg.save(SAVE_PATH)
 
 func load_settings() -> void:
@@ -31,3 +58,5 @@ func load_settings() -> void:
 		return
 	control_mode = cfg.get_value("input", "control_mode", ControlMode.KEYBOARD)
 	coins_total = cfg.get_value("stats", "coins_total", 0)
+	owned_skins.assign(cfg.get_value("cosmetics", "owned_skins", ["default"]))
+	equipped_skin = cfg.get_value("cosmetics", "equipped_skin", "default")
