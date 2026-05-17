63e99360a769a17f602c6beeb76686f81d0e93f3
quvntvn <hulkufe4@gmail.com>
2026-05-17 15:47:32 +0200

    feat(collectible): detection PlayerController + appel Settings.add_coin

---
+++ hyperdive/scripts/collectibles/coin.gd
@@ -0,0 +1,16 @@
+extends Area3D
+class_name Coin
+
+const ROTATION_SPEED: float = 3.0
+
+func _ready() -> void:
+	add_to_group("coins")
+	body_entered.connect(_on_body_entered)
+
+func _process(delta: float) -> void:
+	rotation.y += ROTATION_SPEED * delta
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is PlayerController:
+		Settings.add_coin()
+		queue_free()
